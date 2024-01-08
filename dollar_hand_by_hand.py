fba2a4b29793b363fe2b2c6b5de46475ad37b493
Faeze Afghahi <afghahi.faeze@gmail.com>
2024-01-08 10:05:11 +0400

    upload code for dollar hand by hand

---
+++ dollar_hand_by_hand.py
@@ -0,0 +1,27 @@
+
+"""
+Created on Mon Jan  8 09:40:47 2024
+
+play with dollars
+
+@author: Faeze Afghahi
+"""
+
+import random
+
+random.seed()
+
+people =[]
+for i in range (0,50):
+    people.append(100)
+    
+print(people)
+
+for person1 in range(0,50):
+    person2 = random.randrange(0,50)
+    while people[person2] == 0:
+        person2 = random.randrange(0,50)
+    if people[person1] != 0:
+        print (person1,person2)
+        people[person1] = people[person1]-1
+        people[person2] = people[person2]+1
